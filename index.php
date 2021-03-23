480adc83e161a028a8f9c8d294bd719942f57d43
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2021-03-23 15:27:43 -0600

    Frameworks
    Se agregan los frameworks para trabajar con ellos en la página.

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!-- Optimizar sitio para movil -->
+    <meta name="MobileOptimizer" content="width">
+    <!-- Optimizo el sitio web para mobiles -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Descripción del sitio web o plataforma -->
+    <meta name="description" content="Sistema Online para ayuda y guia de estudiantes, futuros estudiantes del colegio Conalep Naucalpan I, Gestion del plantel a travez de modulos como son: resgistro de asistencias,
+    Reportes de alumnos, Recorrido virtual de las áreas que conforman el plantel, Desarrollo de Juegos interactivos para el aprendizaje de las distintas carreras que ofrece la institución, Carreras tecnicas.">
+    <!-- palabras clave -->
+    <meta name="keywords" content="Sistema Online, Reportes de Asistencia, Recorrido Virtual, Juegos Interactivos, Guia de estudiantes, Conalep Naucalpan I, Gestion de planteles">
+    <!-- autor del sitio web -->
+    <meta name="autor" content="Gatech Asesor & tecnología and Borderline">
+    <!-- Integrar framworks para el desarrollo de el sistema -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/fontello.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <title>Inicio de Sistema | AdminSchool Conalep Naucalpan I</title>
+  </head>
+  <body>
+    <div class="container py-4">
+         <h3 class="text-center text-light py-4"><span class="icon-users"></span> Conactive Conalep Naucalpan I</h3>
+         <div class="row justify-content-center h-100 py-4">
+             <div class="card col-sm-8 col-md-6 col-lg-6 shadow-lg p-3 mb-5 bg-light text-dark rounded">
+                  <p class="text-center text-dark py-3"><span class="icon-lock"><span> Inicio de Sesión</p><hr>
+                  <div class="container text-center">
+                      <div class="py-2">
+                         <div class="container">
+                             <div class="row">
+                                 <div class="card shadow-lg p-3 mb-5 bg-light text-dark rounded text-center col-sm-4">
+                                   <div class="inicio py-1">
+                                       <span class="icon-graduation-cap"></span>
+                                       <p class="text-muted text-center">Alumnos</p>
+                                   </div>
+                                 </div>
+                                   <div class="card shadow-lg p-3 mb-5 bg-light text-dark rounded col-sm-4">
+                                     <div class="inicio py-1">
+                                     <span class="icon-suitcase"></span>
+                                     <p class="text-muted text-center">Administrativo</p>
+                                   </div>
+                                 </div>
+                                 <div class="card shadow-lg p-3 mb-5 bg-light text-dark rounded text-center col-sm-4">
+                                   <div class="inicio py-1">
+                                     <span class="icon-users"></span>
+                                     <p class="text-muted text-center">Docentes</p>
+                                   </div>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                </div>
+            </div>
+         </div>
+    </div>
+  <script src="js/bootstrap.js"></script>
+  </body>
+</html>
